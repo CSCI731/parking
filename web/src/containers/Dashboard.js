b00350afdc5d452f52b7875694f7cabace24b3a7
Justin Li <jli@arising.net>
2018-11-12 09:38:26 -0500

    improved Dashboard layout

--- web/src/containers/Dashboard.js
+++ web/src/containers/Dashboard.js
@@ -3,17 +3,23 @@ import locationActions from "../redux/actions/locations";
 import signActions from '../redux/actions/signs';
 import DashboardView from '../views/Dashboard';
 
-const mapStateToProps = state => ({
-  boro: state.locations.boro,
-  main: state.locations.main,
-  from: state.locations.from,
-  to: state.locations.to,
-  signs: state.signs.signs,
-});
+
+const mapStateToProps = state => {
+  return {
+    menuIsOpen: state.ui.menuIsOpen,
+    boro: state.locations.boro,
+    main: state.locations.main,
+    from: state.locations.from,
+    to: state.locations.to,
+    currentPosition: state.locations.currentPosition,
+    signs: state.signs.signs,
+  };
+};
 
 const mapDispatchToProps = dispatch => ({
   locationsByLatLng: (lat, lng) => dispatch(locationActions.locationsByLatLng(lat, lng)),
   signsByStreet: (boro, mainSt, fromSt, toSt) => dispatch(signActions.signsByStreet(boro, mainSt, fromSt, toSt)),
+  reverseGeocode: (lat, lng) => dispatch(locationActions.reverseGeocode(lat, lng)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(DashboardView);
--- web/src/layouts/Dashboard.js
+++ web/src/layouts/Dashboard.js
@@ -1,36 +1,25 @@
-import { connect } from 'react-redux';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
 import withStyles from '@material-ui/core/styles/withStyles';
 import PropTypes from 'prop-types';
-import Header from '../components/common/Header';
-import Footer from '../components/common/Footer';
-import Sidebar from '../components/common/Sidebar';
+import MenuContainer from '../containers/Menu';
 import DashboardContainer from '../containers/Dashboard';
 
-const mapStateToProps = state => ({
-  ui: state.ui,
-});
-
-const styles = theme => ({
+const styles = () => ({
   root: {
-    flexGrow: 1,
-    zIndex: 1,
-    overflow: 'hidden',
-    position: 'relative',
     display: 'flex',
   },
 });
 
 
 const Dashboard = (props) => {
-  const { ui, classes } = props;
+  const { classes } = props;
   return (
-    <Grid container>
-      <Grid item xs={12} lg={12}>
-        <DashboardContainer />
-      </Grid>
-    </Grid>
+    <div className={classes.root}>
+      <CssBaseline />
+      <MenuContainer />
+      <DashboardContainer />
+    </div>
   );
 };
 
@@ -38,8 +27,7 @@ Dashboard.propTypes = {
   classes: PropTypes.shape({}).isRequired,
 };
 
-export default connect(
-  mapStateToProps,
-)(withStyles(styles, { withTheme: true })(Dashboard));
+export default withStyles(styles)(Dashboard);
+
 
 
