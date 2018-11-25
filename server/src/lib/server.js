0f82a1fea25c745b96e5ea812d9298b3bb51900f
Justin Li <jli@arising.net>
2018-11-24 19:44:44 -0500

    reverted ssl support

--- server/src/lib/server.js
+++ server/src/lib/server.js
@@ -2,14 +2,10 @@ import cors from 'cors';
 import debug from 'debug';
 import morgan from 'morgan';
 import express from 'express';
-import fs from 'fs';
-import http from 'http';
-import https from 'https';
 import { ApolloServer } from 'apollo-server-express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import schema from '../schema';
-
 
 const app = express(morgan('combined'));
 const log = debug('server');
@@ -28,24 +24,8 @@ const corsOptions = {
 
 app.use(bodyParser.json(), cors(corsOptions));
 
-
-const apollo = new ApolloServer(schema);
-apollo.applyMiddleware({ app });
-
-let server;
-if (process.env.HTTPS) {
-  server = https.createServer(
-    {
-      key: fs.readFileSync(process.env.SSL_KEY),
-      cert: fs.readFileSync(process.env.SSL_CERT),
-    },
-    app
-  );
-} else {
-  server = http.create(app);
-}
-
-apollo.installSubscriptionHandlers(server);
+const server = new ApolloServer(schema);
+server.applyMiddleware({ app });
 
 app.all('*', (request, response) => {
   log('Returning a 404 from the catch-all route');
@@ -54,13 +34,13 @@ app.all('*', (request, response) => {
 
 
 export const start = () => {
-  server.listen({ port: PORT }, () => {
+  app.listen(PORT, () => {
     log(`Listening on port: ${PORT}`);
   });
 };
 
 export const stop = () => {
-  app.close(() => {
+  app.close(PORT, () => {
     log(`Shut down on port: ${PORT}`);
   });
 };
