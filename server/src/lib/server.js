49d208a9f62310fb6dac72a482eeffe8d5d088c0
Justin Li <starxml@gmail.com>
2018-11-26 04:52:50 +0000

    loaded ssl wqcertificate

--- server/src/lib/server.js
+++ server/src/lib/server.js
@@ -6,6 +6,7 @@ import express from 'express';
 import fs from 'fs';
 import http from 'http';
 import https from 'https';
+import path from 'path';
 import { ApolloServer } from 'apollo-server-express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
@@ -13,6 +14,7 @@ import schema from '../schema';
 
 const app = express(morgan('combined'));
 app.use(helmet());
+app.use(express.static(path.join(__dirname , '../../static'), {dotfiles: 'allow'}));
 
 const log = debug('server');
 
@@ -38,8 +40,8 @@ if (process.env.HTTPS === '1') {
   console.log('HTTPS', process.env.HTTPS);
   server = https.createServer(
     {
-      key: fs.readFileSync(process.env.SSL_KEY),
-      cert: fs.readFileSync(process.env.SSL_CERT),
+      key: fs.readFileSync(process.env.SSL_KEY, 'utf8'),
+      cert: fs.readFileSync(process.env.SSL_CERT, 'utf8'),
     },
     app
   );
