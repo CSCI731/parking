39f1d659184aa17674d7fb488d48c5bdca535ece
Justin Li <jli@arising.net>
2018-11-30 22:41:54 -0500

    loaded auth graphql schema

--- server/src/schema/resolvers/index.js
+++ server/src/schema/resolvers/index.js
@@ -1,11 +1,13 @@
 import { mergeResolvers } from 'merge-graphql-schemas';
 import signResolvers from './sign';
 import locationResolvers from './location';
+import authResolvers from './auth';
 
 
 const resolvers = [
   signResolvers,
   locationResolvers,
+  authResolvers,
 ];
 
 export default mergeResolvers(resolvers);
--- server/src/schema/types/index.js
+++ server/src/schema/types/index.js
@@ -1,12 +1,16 @@
 import { mergeTypes } from 'merge-graphql-schemas';
 import baseTypes from './base.graphql';
 import signTypes from './sign.graphql';
+import userTypes from './user.graphql';
+import authTypes from './auth.graphql';
 import locationTypes from './location.graphql';
 
 const typeDefs = mergeTypes([
   baseTypes,
   signTypes,
   locationTypes,
+  userTypes,
+  authTypes,
 ]);
 
 
