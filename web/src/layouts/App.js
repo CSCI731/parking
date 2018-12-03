3ee8702b7ffb1143225e2628605d7a6947e5cbf7
Justin Li <jli@arising.net>
2018-12-03 09:57:15 -0500

    send graphql request with authentication header

--- web/src/layouts/App.js
+++ web/src/layouts/App.js
@@ -6,6 +6,7 @@
  */
 
 import React from 'react';
+import Store from 'store';
 import { Provider } from 'react-redux';
 import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
@@ -15,14 +16,27 @@ import DashboardLayout from './Dashboard';
 import LoginLayout from './LoginLayout';
 import PrivateRoute from './PrivateRoute';
 import AdminLayout from './Admin';
+import { setContext } from 'apollo-link-context';
 import PublicRoute from "./PublicRoute";
 
 const httpLink = new HttpLink({
   uri: process.env.REACT_APP_GRAPHQL_SERVER_URI,
 });
 
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = Store.get('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
