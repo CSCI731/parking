66b82b8b1ba333d5e5ee743c5a754d3e4dcc5155
Justin Li <jli@arising.net>
2018-12-03 10:18:43 -0500

    add mutation for sings

--- server/src/schema/resolvers/sign/mutation.js
+++ server/src/schema/resolvers/sign/mutation.js
@@ -1,4 +1,9 @@
+import SignModel from 'model/sign';
+
 const Mutation = {};
 
+Mutation.addSign = async (parent, { input }, { user }) => {
+  return await SignModel.insertOne(input);
+};
 
 export default Mutation;
